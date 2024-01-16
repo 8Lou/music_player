7947b45276729dacd0d8ac090343f47a57b5739a
8Lou <lou8@mail.ru>
2024-01-16 19:53:00 +0300

    module.scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 import MainPage from "./pages/MainPage/MainPage";
+import style from "./global.module.scss";
 
-const App = () => <MainPage />;
+const App = () => (
+  <div className={style.wrapper}>
+    <MainPage />
+
+  </div>
+);
 
 export default App;
--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,3 +1,25 @@
-const MainPage = () => <>Main page</>
+
+import trackList from "../../assets/trackList";
+
+import style from "./mainPage.module.scss";
+
+const MainPage = () => {
+    return (
+        <div className={style.search}>
+            {/* <Input
+                className={style.input}
+                placeholder="Поиск треков"
+                onChange={handleChange} */}
+            {/* /> */}
+            <div className={style.list}>
+                {trackList.map((track) => (
+                    <div>{JSON.stringify(track)}</div>
+                ))}
+            </div>
+        </div>
+    );
+}
+
+
 
 export default MainPage;
